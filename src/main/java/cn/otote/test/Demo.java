bd2f7c89cc9fbef5a0f6342118cd95c0f399773e
otote <ljb12138@gmail.com>
2018-11-14 20:57:36 +0800

    Initial commit

---
+++ src/main/java/cn/otote/test/Demo.java
@@ -0,0 +1,11 @@
+package cn.otote.test;
+
+/**
+ * @author otote
+ * Created on 2018/11/14 20:56.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
